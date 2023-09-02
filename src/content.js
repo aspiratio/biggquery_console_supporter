a55c8325102cbf7476e21beb48aa9990cd1d6f23
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-09-02 14:49:03 +0900

    utility.jsに切り出した関数をインポートできないため、content.jsに統合

--- src/content.js
+++ src/content.js
@@ -1,5 +1,3 @@
-const utility = require("./utility.js");
-
 document.body.addEventListener("click", (event) => {
   // 取得したいカスタムタグ（この配下にアイコンのボタンとバリデーションメッセージが含まれる）
   const targetTag = "query-validation-status";
@@ -10,7 +8,7 @@ document.body.addEventListener("click", (event) => {
   クリックされた要素より上位の query-validation-status タグを取得する
   これによって、複数存在する中から目的のタグだけを取得できる
   */
-  const queryValidationStatusElement = utility.findParentElementByTagName(
+  const queryValidationStatusElement = findParentElementByTagName(
     clickedElement,
     targetTag
   );
@@ -22,5 +20,42 @@ document.body.addEventListener("click", (event) => {
     ".cfc-truncated-text, .ng-star-inserted"
   );
 
-  utility.copyElementTextToClipboard(messageElement);
+  copyElementTextToClipboard(messageElement);
 });
+
+/**
+ * 上位の指定した要素を再起的に探す関数
+ * @param {HTMLElement} element DOM要素
+ * @param {string} targetTagName 取得したいタグの名前
+ * @return {HTMLElement|null} 要素が見つかった場合はHTMLElement、見つからない場合はnullを返す
+ */
+const findParentElementByTagName = (element, targetTagName) => {
+  // 最上位に行き着くと関数が終了する
+  if (!element) return null;
+
+  /*
+	要素名を比較するために念の為、小文字に統一する
+	HTMLはタグ名を大文字小文字で区別しないが、JSの .tagName プロパティによって取得されるタグ名は大文字小文字が区別されるため
+	*/
+  if (element.tagName.toLowerCase() === targetTagName.toLowerCase()) {
+    return element;
+  }
+
+  return findParentElementByTagName(element.parentElement, targetTagName);
+};
+
+/**
+ * 指定した要素内のテキストをクリップボードにコピーする
+ * @param {HTMLElement} element DOM要素
+ */
+
+const copyElementTextToClipboard = (element) => {
+  // ChromeでClipboard APIに対応しなくなった場合に、エラーを回避するため
+  if (!navigator.clipboard) {
+    return;
+  }
+
+  if (!element) return;
+
+  navigator.clipboard.writeText(element.textContent);
+};
